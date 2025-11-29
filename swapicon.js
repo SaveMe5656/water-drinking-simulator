a1162ed1d2c80619e97fe610a98d00bfb68bab7b
CommandCreep <133177672+SaveMe5656@users.noreply.github.com>
2025-11-29 12:41:58 -0600

    Create swapicon.js
    add alt icon enabling

---
+++ swapicon.js
@@ -0,0 +1,5 @@
+function swapIcon() {
+	document.getElement("icon").setAttribute("href", "assets/waterd33.ico");
+}
+
+console.log("For anyone who's looking to create a shortcut, there's an alternative icon that can be enabled. You can run swapIcon() to do so.")
